06c7ad41b2178825d7cdf85567bd8b408afed677
Alex <themostequity@bk.ru>
2020-08-02 17:44:43 +0300

    make view

---
+++ resources/views/view.blade.php
@@ -0,0 +1,106 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Task</title>
+	<link rel="stylesheet" type="text/css" href="css/main.css">
+</head>
+<body>
+	<header>
+		<div class="header_section">
+			<div class="header_section__item">
+				<div class="header_section__item_phonenumber">Телефон: (495)340-94-71</div>
+				<div class="header_section__item_email">Email: <u>info@future-group.ru</u></div>
+			</div>
+			<div class="header_section__item header_section__item_comment">
+				Комментарии
+			</div>
+		</div>
+		<div class="header_section">
+			<img class="header_section_logo" src="images/logo.png">
+		</div>
+	</header>
+
+
+
+
+
+	<main>
+		<div class="comments">
+			<div class="comments__item">
+				<div class="name_time">
+					<div class="name">Савва</div>
+					<div class="time_day">
+						<div class="time">18:05</div>
+						<div class="day">07.02.2014</div>
+					</div>
+				</div>
+				<div class="comment">
+					Спасибо за Ваше тестовое задание.Оно, действительно изумительное.
+				</div>
+			</div>
+			<div class="comments__item">
+				<div class="name_time">
+					<div class="name">Евдоким</div>
+					<div class="time_day">
+						<div class="time">17:42</div>
+						<div class="day">07.02.2014</div>
+					</div>
+				</div>
+				<div class="comment">
+					Если включить мозги, то задание элементарно, Ватсон!
+				</div>
+			</div>
+			<div class="comments__item">
+				<div class="name_time">
+					<div class="name">Артемий</div>
+					<div class="time_day">
+						<div class="time">16:10</div>
+						<div class="day">07.02.2014</div>
+					</div>
+				</div>
+				<div class="comment">
+					Почему такое сложное задание???Мне кажется, нужно быть первоклассным программистом, чтобы выполнить его :(
+				</div>
+			</div>
+		</div>
+		<div class="form_section">
+			<h2>Оставить комментарий</h2>
+			<div class="form_section__item">
+				<label class="form_section__item_desc">Ваше имя</label>
+				<input class="form_section__item_input_name" type="text" name="name" placeholder="Герасим">
+			</div>
+			<div class="form_section__item">
+				<label class="form_section__item_desc">Ваш комментарий</label>
+				<textarea class="form_section__item_input_text"></textarea>
+				<input class="form_section__item_button" type="submit" name="button" value="Отправить">
+			</div>
+		</div>
+	</main>
+
+
+
+
+
+
+	<footer>
+		<div class="footer__item">
+			<div class="footer__item_logo">
+				<img class="header_section_logo" src="images/logo.png">
+			</div>
+		</div>
+		<div class="footer__item">
+			<div class="footer__item_info_rights">
+				<div class="footer__item_info">
+					<div class="footer__item_phonenumber">Телефон (495)340-94-71</div>
+					<div class="footer__item_email">Email: <u>info@future-group.ru</u></div>
+					<div class="footer__item_adress">Адрес:<u> 115088 Москва, ул. 2-я Машиностроения, д. 7 стр.1</u></div>
+				</div>
+				<div class="footer__item_rights">
+					© 2010 - 2014 Future. Все права защищены
+				</div>
+			</div>
+		</div>
+	</footer>
+</body>
+</html>
